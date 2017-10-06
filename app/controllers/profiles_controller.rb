1b4fbe228487743386547fc54826a667a446352f
Eric Lobatos <eric_lobatos@yahoo.com>
2017-10-06 04:14:06 +0000

    Made improvements to user profile pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,12 +4,12 @@ class ProfilesController < ApplicationController
     
     def new
         # a form where a user can create a profile
-        @user = User.find(params[:user_id])
+        @user = User.find( params[:user_id] )
         @profile = Profile.new
     end
     
     def create 
-        @user = User.find(params[:user_id])
+        @user = User.find( params[:user_id] )
         @profile = @user.build_profile(profile_params)
         if @profile.save
             redirect_to user_path(params[:user_id])
@@ -20,16 +20,16 @@ class ProfilesController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:user_id])
+        @user = User.find( params[:user_id] )
         @profile = @user.profile
     end
     
     def update
-        @user = User.find(params[:user_id])
+        @user = User.find( params[:user_id] )
         @profile = @user.profile
         if @profile.update_attributes(profile_params)
             flash[:success] = "Profile Updated!"
-            redirect_to user_path(params[:user_id])
+            redirect_to user_path( params[:user_id] )
         else
             render action: :edit
         end
@@ -37,11 +37,11 @@ class ProfilesController < ApplicationController
     
     private
     def profile_params
-        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
     
     def only_current_user
-        @user = User.find(params[:user_id])
+        @user = User.find( params[:user_id] )
         redirect_to(root_url) unless @user == current_user
     end
 end
